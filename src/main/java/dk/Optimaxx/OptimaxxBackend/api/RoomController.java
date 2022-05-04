dcfa2e7d21bdb9b72fa287bafcefda52851a9ee4
frederikmahipal <fred88n5@stud.kea.dk>
2022-05-04 10:40:18 +0200

    endpointzzzz (with fix)

--- src/main/java/dk/Optimaxx/OptimaxxBackend/api/RoomController.java
+++ src/main/java/dk/Optimaxx/OptimaxxBackend/api/RoomController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/movie")
+@RequestMapping("/api/room")
 @CrossOrigin
 @AllArgsConstructor
 public class RoomController {
