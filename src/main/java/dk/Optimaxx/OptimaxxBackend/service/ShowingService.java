67214a6da2a3b99fa8a0a244cab3ef90f8938395
Simon Gredal <simon.gredal@icloud.com>
2022-05-09 11:25:35 +0200

    Add method to return SeatResponses with reserved status

--- src/main/java/dk/Optimaxx/OptimaxxBackend/service/ShowingService.java
+++ src/main/java/dk/Optimaxx/OptimaxxBackend/service/ShowingService.java
@@ -1,11 +1,15 @@
 package dk.Optimaxx.OptimaxxBackend.service;
 
+import dk.Optimaxx.OptimaxxBackend.DTO.SeatResponse;
 import dk.Optimaxx.OptimaxxBackend.DTO.ShowingResponse;
 import dk.Optimaxx.OptimaxxBackend.entity.Seat;
+import dk.Optimaxx.OptimaxxBackend.entity.Reservation;
+import dk.Optimaxx.OptimaxxBackend.entity.Showing;
 import dk.Optimaxx.OptimaxxBackend.repository.ShowingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -27,5 +31,27 @@ public class ShowingService {
         ShowingResponse showingResponse = getShowingById(id);
         // TODO: compare and exclude with reserved seats somewhere, but where? ;-;
         return showingResponse.getRoom().getSeats();
+    }
+
+    public List<SeatResponse> getAllSeatsByShowingId(Long id) {
+        Showing showing = showingRepository.getById(id);
+
+        List<Seat> unknownSeats = showing.getRoom().getSeats().stream().toList();
+
+        List<Long> reservedSeatIds = showing.getReservations()
+                .stream()
+                .map(Reservation::getSeats)
+                .flatMap(Collection::stream)
+                .map(Seat::getId)
+                .toList();
+
+        List<SeatResponse> knownSeatResponses = unknownSeats.stream()
+                .map(SeatResponse::of)
+                .peek(seatResponse -> {
+                    if (reservedSeatIds.contains(seatResponse.getId())) { seatResponse.setReserved(true); }
+                })
+                .toList();
+
+        return knownSeatResponses;
     }
 }
