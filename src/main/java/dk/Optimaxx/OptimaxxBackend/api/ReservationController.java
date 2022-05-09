39a1cc4f9e4de760870f7684801237ca98b44d9a
mark3010 <marklarsen96@gmail.com>
2022-05-09 09:25:49 +0200

    reservations n shtuff

---
+++ src/main/java/dk/Optimaxx/OptimaxxBackend/api/ReservationController.java
@@ -0,0 +1,29 @@
+package dk.Optimaxx.OptimaxxBackend.api;
+
+import dk.Optimaxx.OptimaxxBackend.DTO.ReservationResponse;
+import dk.Optimaxx.OptimaxxBackend.service.ReservationService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservations")
+@CrossOrigin
+@AllArgsConstructor
+public class ReservationController {
+    private final ReservationService reservationService;
+
+    @GetMapping
+    public List<ReservationResponse> getMovies(Pageable pageable){
+        return reservationService.getAllReservations(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ReservationResponse getReservations(@PathVariable Long id){
+        return reservationService.getReservationById(id);
+    }
+
+
+}
