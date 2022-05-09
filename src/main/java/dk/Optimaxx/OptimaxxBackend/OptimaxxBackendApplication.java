79a66f3aaa3750ab9646955402391e9532fc6546
Simon Gredal <simon.gredal@icloud.com>
2022-05-09 10:22:10 +0200

    Add shortcut to swagger ui

--- src/main/java/dk/Optimaxx/OptimaxxBackend/OptimaxxBackendApplication.java
+++ src/main/java/dk/Optimaxx/OptimaxxBackend/OptimaxxBackendApplication.java
@@ -2,9 +2,18 @@ package dk.Optimaxx.OptimaxxBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Controller
 public class OptimaxxBackendApplication {
+
+    @GetMapping
+    public static String index() {
+        return "redirect:/swagger-ui/index.html";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(OptimaxxBackendApplication.class, args);
