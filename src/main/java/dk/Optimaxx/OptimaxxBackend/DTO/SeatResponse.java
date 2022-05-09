05898e1a1d7e5706564bbb33c8c8e3f7cf88c555
Simon Gredal <simon.gredal@icloud.com>
2022-05-09 10:42:33 +0200

    Add SeatResponse

---
+++ src/main/java/dk/Optimaxx/OptimaxxBackend/DTO/SeatResponse.java
@@ -0,0 +1,36 @@
+package dk.Optimaxx.OptimaxxBackend.DTO;
+
+import dk.Optimaxx.OptimaxxBackend.entity.Seat;
+import dk.Optimaxx.OptimaxxBackend.entity.Showing;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+public class SeatResponse {
+    private Long id;
+    private Integer number;
+    private Integer row;
+    private String type;
+    private Boolean reserved;
+
+    private SeatResponse(Seat seat) {
+        this.id = seat.getId();
+        this.number = seat.getSeatNumber();
+        this.row = seat.getSeatRow();
+        this.type = seat.getType();
+        this.reserved = false;
+    }
+
+    public static SeatResponse of(Seat entity) {
+        return new SeatResponse(entity);
+    }
+
+    public static List<SeatResponse> of(Collection<Seat> entities) {
+        return entities.stream().map(SeatResponse::new).toList();
+    }
+}
