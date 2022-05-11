0ffb83d41e9d836596cfc1f358bc88878ac3330d
Malthe <malthegram22@gmail.com>
2022-05-11 16:57:20 +0200

    Formatted duration for showingResponse

--- src/main/java/dk/Optimaxx/OptimaxxBackend/DTO/ShowingResponse.java
+++ src/main/java/dk/Optimaxx/OptimaxxBackend/DTO/ShowingResponse.java
@@ -1,7 +1,9 @@
 package dk.Optimaxx.OptimaxxBackend.DTO;
 
+import dk.Optimaxx.OptimaxxBackend.entity.Movie;
 import dk.Optimaxx.OptimaxxBackend.entity.Room;
 import dk.Optimaxx.OptimaxxBackend.entity.Showing;
+import dk.Optimaxx.OptimaxxBackend.utilities.DurationFormatter;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.Setter;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
@@ -18,13 +21,13 @@ import java.util.Set;
 public class ShowingResponse {
     private Long id;
     private RoomResponse room;
-    private LocalDateTime startTime;
+    private String startTime;
     private Integer basePrice;
 
     private ShowingResponse(Showing showing) {
         this.id = showing.getId();
         this.room = showing.getRoom() == null ? null : RoomResponse.of(showing.getRoom());
-        this.startTime = showing.getStartTime();
+        this.startTime = DurationFormatter.formatLocalDateTime(showing.getStartTime());
         this.basePrice = showing.getBasePrice();
     }
 
--- src/main/java/dk/Optimaxx/OptimaxxBackend/utilities/DurationFormatter.java
+++ src/main/java/dk/Optimaxx/OptimaxxBackend/utilities/DurationFormatter.java
@@ -2,9 +2,14 @@ package dk.Optimaxx.OptimaxxBackend.utilities;
 
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 
 public class DurationFormatter {
     public static String format(Duration duration) {
         return "%dh %dm".formatted(duration.toHours(), duration.toMinutesPart());
+    }
+
+    public static String formatLocalDateTime(LocalDateTime time) {
+        return "%d:%d".formatted(time.getHour(),time.getMinute());
     }
 }
